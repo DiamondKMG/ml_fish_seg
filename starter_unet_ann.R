8049d1eff399f00db8bc8ce09c29c573254cee7b
stnava <stnava@gmail.com>
2020-02-07 19:40:22 +0100

    ENH: working net

--- starter_unet_ann.R
+++ starter_unet_ann.R
@@ -5,7 +5,7 @@ library( keras )
 # library(tensorflow)
 
 #path to segmented reference images that comes with repo
-path = '~/ml_fish_seg/fish_segmentations'
+path = './fish_segmentations'
 
 seg_files = dir(patt='nii', path = path, full.names = T) #segmentation files end in nii
 img_files = dir(patt='jpg', path = path, full.names = T) #original RGB image files end in jpg
@@ -23,7 +23,7 @@ for (i in 1:length(seg_files)) reduced_segs [[i]] = resampleImage(segmentations[
 for (i in 1:length(img_files)) reduced_imgs [[i]] = resampleImage(images[[i]], c(256, 256), useVoxels = TRUE, interpType = 'linear')
 
 #define segments
-segmentationLabels <- c( 1, 2, 3 )
+segmentationLabels <- c( 0, 1, 2, 3 )
 numberOfLabels <- length( segmentationLabels )
 initialization <- paste0( 'KMeans[', numberOfLabels, ']' )
 trainingBatchSize = length(reduced_imgs)
@@ -42,7 +42,7 @@ for( i in seq_len( trainingBatchSize ) )
     X_train[i,,, 1:3] <- as.array( reduced_imgs[[i]] ) #populate with images
     Y_train[i,,] <- as.array( reduced_segs[[i]] ) #populate with segmentation
     }
-  
+
   Y_train <- encodeUnet( Y_train, segmentationLabels ) #tags segmentations with segmentation labels (1-3)
 
   # Perform a simple normalization
@@ -50,8 +50,8 @@ for( i in seq_len( trainingBatchSize ) )
   X_train <- ( X_train - mean( X_train ) ) / sd( X_train )
 
 
-model <- createUnetModel2D( c( dim( domainImage ), 1 ),
-    numberOfOutputs = numberOfLabels ) #create model with first image in test set
+model <- createUnetModel2D( c( dim( domainImage ), 3 ),
+    numberOfOutputs = 4 ) #create model with first image in test set
 
 model %>% compile( loss = loss_categorical_crossentropy,
     optimizer = optimizer_adam( lr = 0.0001 )  ) #configures a Keras model for training
@@ -59,7 +59,7 @@ model %>% compile( loss = loss_categorical_crossentropy,
 track <- model %>% fit( X_train, Y_train,
            epochs = 100, batch_size = 4, verbose = 1, shuffle = TRUE,
            validation_split = 0.2 ) #Trains the model for a fixed number of epochs (iterations on a dataset).
-          #This last bit is currently struggling bc the dimentions of the image (x) and segmentation (y) arrays have different dimentions 
+          #This last bit is currently struggling bc the dimentions of the image (x) and segmentation (y) arrays have different dimentions
           #work on fixing this tomorrow when not brain dead - also save this whole thing as a rmd tomorrow here and in my helens drive
 
 #at this point the model has been trained but not tested on any new data
